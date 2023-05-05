21319da53b40d58d64867ca8eafbaa31eb78a676
Alexey Lisov <lisov.alex@gmail.com>
2023-05-05 11:30:55 +0100

    Initial Commit

---
+++ BuyTicketsChallengeApp/BuyTicketsChallengeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyTicketsChallengeAppApp.swift
+//  BuyTicketsChallengeApp
+//
+//  Created by Alexey Lisov on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BuyTicketsChallengeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
