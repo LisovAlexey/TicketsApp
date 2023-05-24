bd686faefec4946080bd699b7242f22e72485eff
Alexey Lisov <lisov.alex@gmail.com>
2023-05-24 16:16:33 +0100

    Last commit

--- BuyTicketsChallengeApp/Core/domain/model/Event.swift
+++ BuyTicketsChallengeApp/Core/domain/model/Event.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Event: Decodable {
-    
-    static var mock: Event = Event(title: "Event name", description: "Event description, long long long long long long long long long long long long long long long long usually", date: Date.now, price: 45, address: "Strange street 25", contactPhone: "546-005-06")
-    
+    // swiftlint: disable line_length
+    static var mock: Event = Event(title: "Event name",
+                                   description: "Event description, long long long long long long long long long long long long long long long long usually", date: Date.now, price: 45, address: "Strange street 25", contactPhone: "546-005-06")
     var id: UUID?
     var title: String
     var description: String
@@ -19,5 +19,3 @@ struct Event: Decodable {
     var address: String
     var contactPhone: String
 }
-
-
